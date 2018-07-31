e33efda44d572aa83820cb1cd1c497d6ecbf9b0b
artemis1688 <artemis01168@gmail.com>
2018-07-31 21:55:41 +0800

    Update 2018-07-31T13:55:38.814Z

---
+++ js/all.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let newFuntion = () => {
+    console.log('today is Friday');
+};
+
+newFuntion();
+
+// var newFuntion(){
+//     console.log('Good Design');
+// }
+
+// newFuntion()
+let newFuntion2 = () => {
+    console.log('Have a good day');
+};
+
+newFuntion2();
+//# sourceMappingURL=all.js.map
